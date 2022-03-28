a05f9efe4daf7f8a186fd426cd1e5de69c1812cd
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-03-28 12:40:33 -0700

    use system.file to locate config

--- R/script-helpers.R
+++ R/script-helpers.R
@@ -38,7 +38,7 @@ get_config <- function(value, config) {
   config::get(
     value = value,
     config = config,
-    #file = system.file("config.yml", package = "cleanAD")
-    file = here::here("inst/config.yml")
+    file = system.file("config.yml", package = "cleanAD")
+    #file = here::here("inst/config.yml")
   )
 }
