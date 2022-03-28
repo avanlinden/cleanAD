0c8b2d326b6447f6224bf64f85579832bb416a36
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-03-28 09:54:44 -0700

    Update update_table.sh

--- update_table.sh
+++ update_table.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-chmod +x ./inst/scripts/generate-table-ad.R 
-
-Rscript ./inst/scripts/generate-table-ad.R --config $1 --auth_token $2
+Rscript cleanAD/inst/scripts/generate-table-ad.R --config $1 --auth_token $2
